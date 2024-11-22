55a9335232c445f7004344542af19d3bf08063b2
clemdeboisfleury <deboisfleuryclement@gmail.com>
2024-11-22 12:10:24 +0100

    1

---
+++ data.py
@@ -0,0 +1,44 @@
+
+
+
+horaires_patus = {
+    "lundi" : "11:00 - 14:30 ",
+    "mardi" : "11:00 - 14:30 ",
+    "mercredi" : "11:00 - 14:30 ",
+    "jeudi" : "11:00 - 17:30 ",
+    "vendredi" : "11:00 - 17:30 ",
+    "samedi" : "Fermé",
+    "dimanche" : "Fermé",    
+}
+
+menus_patus = {}
+
+patus_paninus = {
+    "nom" : "Patus Paninus",
+    "adresse" : "12 rue du Parchamp",
+    "horaires" : horaires_patus,
+    "avis" : 4.8,      
+    "menus" : menus_patus
+}
+
+
+la_casa_del_panini = {
+    "nom" : "La casa del panini",
+    "adresse" : "79 Avenue Jean Baptiste Clement",
+    "horaires" : horaires_casa,
+    "avis" : 4.8,      
+    "menus" : menus_casa #definir
+}
+
+
+grenier_à_pain = {
+    "nom" : "Patus Paninus",
+    "adresse" : "12 rue du Parchamp",
+    "horaires" : horaires_patus,
+    "avis" : 4.8,      
+    "menus" : menus_patus
+} 
+
+
+data = [ patus_paninus, la_casa_del_panini, grenier_à_pain ]
+
