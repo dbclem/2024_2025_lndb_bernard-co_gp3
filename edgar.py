6f9e380fdbb414a21cb009e3cd91baa337cffbcc
Edgar Barioz <bariozedgar@gmail.com>
2025-02-27 23:41:14 +0100

    j'ai enlever les fonction dans les fonctions mais y a un prblm

--- edgar.py
+++ edgar.py
@@ -4,11 +4,82 @@ from main import global_liste_commande_operateur
 def destroy_all_widgets(frame):
     for widget in frame.winfo_children():
         widget.destroy()
-        
-# global_liste_commande_operateur = ["commande1", "commande2", "commande3", "commande4", "commande5"]
+
 global_liste_en_cours = []
 global_list_commande_terminees = []
 
+def refuser_commande(commande):
+    global_liste_commande_operateur.remove(commande)
+    print(global_liste_commande_operateur)
+
+def accepter_commande(commande):
+    if commande in global_liste_en_cours:
+        global_list_commande_terminees.append(commande)
+        global_liste_en_cours.remove(commande)
+    else:
+        global_liste_en_cours.append(commande)
+        global_liste_commande_operateur.remove(commande)
+    print(global_liste_en_cours)
+    print(global_list_commande_terminees)
+
+def add_commandes(demande_frame, process_frame):
+    print(global_liste_commande_operateur)
+    for commande in global_liste_commande_operateur:
+        if "plat" not in commande:
+            text_commande = f"{commande['name']} - {commande['price']} €"
+        else:
+            text_commande = " \n ".join([f"{value} $" if key == "price" else f"{value}" for key, value in commande.items() if key != "temps"])
+
+        commande_frame = Frame(demande_frame, bg="#1A355B", width=200, height=200, highlightbackground="black", highlightcolor="black", highlightthickness=1)
+        commande_frame.pack(pady=10)
+
+        commande_label = Label(commande_frame, text=text_commande, font=("Helvetica", 10), bg="#1A355B", fg="white", highlightbackground="black", highlightcolor="black", highlightthickness=1)
+        commande_label.pack(pady=10)
+
+        accepter_button = Button(commande_frame, text="Accepter", font=("Helvetica", 10), bg="#1A355B", fg="white",
+                                 highlightbackground="black", highlightcolor="black", highlightthickness=1,
+                                 command=lambda commande=commande: [accepter_commande(commande), destroy_all_widgets(demande_frame),
+                                                                    destroy_all_widgets(process_frame), add_commandes(demande_frame, process_frame), add_commandes_en_cours(process_frame)])
+        accepter_button.pack(pady=10, side=RIGHT)
+
+        refuser_button = Button(commande_frame, text="Refuser", font=("Helvetica", 10), bg="#1A355B", fg="white",
+                                highlightbackground="black", highlightcolor="black", highlightthickness=1,
+                                command=lambda commande=commande: [refuser_commande(commande), destroy_all_widgets(demande_frame), add_commandes(demande_frame, process_frame)])
+        refuser_button.pack(pady=10, side=LEFT)
+
+def add_commandes_en_cours(process_frame):
+    global global_liste_en_cours
+    for commande in global_liste_en_cours:
+        if "plat" not in commande:
+            text_commande = f"{commande['name']} - {commande['price']} €"
+        else:
+            text_commande = " \n ".join([f"{value} $" if key == "price" else f"{value}" for key, value in commande.items() if key != "temps"])
+
+        en_cours_frame = Frame(process_frame, bg="#1A355B", width=200, height=200, highlightbackground="black", highlightcolor="black", highlightthickness=1)
+        en_cours_frame.pack(pady=10)
+
+        en_cours_label = Label(en_cours_frame, text=text_commande, font=("Helvetica", 10), bg="#1A355B", fg="white", highlightbackground="black", highlightcolor="black", highlightthickness=1)
+        en_cours_label.pack(pady=10, padx=20)
+
+        terminer_button = Button(en_cours_frame, text="Terminer", font=("Helvetica", 10), bg="#1A355B", fg="white",
+                                 highlightbackground="black", highlightcolor="black", highlightthickness=1,
+                                 command=lambda commande=commande: [accepter_commande(commande), destroy_all_widgets(process_frame),
+                                                                    destroy_all_widgets(en_cours_frame), add_commandes_en_cours(process_frame), add_commandes_terminees()])
+        terminer_button.pack(pady=10)
+
+def add_commandes_terminees():
+    global global_list_commande_terminees
+    for commande in global_list_commande_terminees:
+        if "plat" not in commande:
+            text_commande = f"{commande['name']} - {commande['price']} €"
+        else:
+            text_commande = " \n ".join([f"{value} $" if key == "price" else f"{value}" for key, value in commande.items() if key != "temps"])
+
+        terminees_frame = Frame(commande_frame, bg="#1A355B", width=200, height=200, highlightbackground="black", highlightcolor="black", highlightthickness=1)
+        terminees_frame.pack(pady=10)
+
+        terminees_label = Label(terminees_frame, text=text_commande, font=("Helvetica", 10), bg="#1A355B", fg="white", highlightbackground="black", highlightcolor="black", highlightthickness=1)
+        terminees_label.pack(pady=30, padx=20)
 
 def main_operateur_window():
     """
@@ -32,7 +103,6 @@ def main_operateur_window():
     label = Label(main_frame, text="Interface Opérateur", font=("Helvetica", 20), bg="#0d2c56", fg="white")
     label.pack(pady=20)
 
-
     # creation de la frame des demandes initial
     demande_frame_intial = Frame(main_frame, bg="#0d2c56", width=200, height=200, highlightbackground="black", highlightcolor="black", highlightthickness=1)
     demande_frame_intial.pack(side=LEFT, expand=True)
@@ -45,7 +115,6 @@ def main_operateur_window():
     demande_frame = Frame(demande_frame_intial, bg="#0d2c56", width=200, height=200)
     demande_frame.pack(expand=True)
 
-    
     # creation de la frame des process initial
     process_frame_initial = Frame(main_frame, bg="#0d2c56", width=200, height=200, highlightbackground="black", highlightcolor="black", highlightthickness=1)
     process_frame_initial.pack(side=LEFT, expand=True)
@@ -57,7 +126,6 @@ def main_operateur_window():
     # creation de la frame des process
     process_frame = Frame(process_frame_initial, bg="#0d2c56", width=200, height=200)
     process_frame.pack(side=LEFT, expand=True) 
-
 
     # creation de la frame des commandes initial
     commande_frame_initial = Frame(main_frame, bg="#0d2c56", width=200, height=200, highlightbackground="black", highlightcolor="black", highlightthickness=1)
@@ -72,103 +140,10 @@ def main_operateur_window():
     commande_frame.pack(side=LEFT, expand=True)
 
     # creation d'un bouton refresh
-    refresh_button = Button(main_frame, text="Refresh", font=("Helvetica", 10), bg="#1A355B", fg="white", command=lambda: [destroy_all_widgets(demande_frame), add_commandes()])
+    refresh_button = Button(main_frame, text="Refresh", font=("Helvetica", 10), bg="#1A355B", fg="white", command=lambda: [destroy_all_widgets(demande_frame), add_commandes(demande_frame, process_frame)])
     refresh_button.place(relx=0.1, rely=0.1, anchor=CENTER)
 
-
-    # creation de la fonction qui ajoute les commandes demandées
-    def add_commandes ():
-        print(global_liste_commande_operateur)
-        global_liste_commande_operateur
-        for commande in global_liste_commande_operateur :
-            
-            if "plat" not in commande : 
-                text_commande = f"{commande['name']} - {commande['price']} €"
-            else :
-                # commande faite a partir de chatgpt qui permet de modifier réactivement le texte
-                text_commande = " \n ".join([f"{value} $" if key == "price" else f"{value}" for key, value in commande.items() if key != "temps"])
-
-            
-            commande_frame = Frame(demande_frame, bg="#1A355B", width=200, height=200, highlightbackground="black", highlightcolor="black", highlightthickness=1)
-            commande_frame.pack(pady=10)
-
-            commande_label = Label(commande_frame, text=text_commande, font=("Helvetica", 10), bg="#1A355B", fg="white", highlightbackground="black", highlightcolor="black", highlightthickness=1)
-            commande_label.pack(pady=10)
-
-            accepter_button = Button(commande_frame, text="Accepter", font=("Helvetica", 10), bg="#1A355B", fg="white"
-                                    , highlightbackground="black", highlightcolor="black", highlightthickness=1
-                                    , command=lambda commande=commande: [accepter_commande(commande), destroy_all_widgets(demande_frame)
-                                    , destroy_all_widgets(process_frame), add_commandes(), add_commandes_en_cours()])
-            accepter_button.pack(pady=10,side=RIGHT)
-
-            refuser_button = Button(commande_frame, text="Refuser", font=("Helvetica", 10), bg="#1A355B", fg="white"
-                                    , highlightbackground="black", highlightcolor="black", highlightthickness=1
-                                    , command=lambda commande=commande: [refuser_commande(commande), destroy_all_widgets(demande_frame), add_commandes()])
-            refuser_button.pack(pady=10,side=LEFT)
-
-    add_commandes()
-
-    def refuser_commande(commande):
-        global_liste_commande_operateur.remove(commande)
-        print(global_liste_commande_operateur)
-
-    def accepter_commande(commande):
-        if commande in global_liste_en_cours :
-            global_list_commande_terminees.append(commande)
-            global_liste_en_cours.remove(commande)
-        else :
-            global_liste_en_cours.append(commande)
-            global_liste_commande_operateur.remove(commande)
-        print(global_liste_en_cours)
-        print(global_list_commande_terminees)
-
-    # creation de la fonction qui ajoute les commandes en cours
-    def add_commandes_en_cours ():
-        global global_liste_en_cours
-        for commande in global_liste_en_cours :
-            if "plat" not in commande : 
-                text_commande = f"{commande['name']} - {commande['price']} €"
-            else :
-                # commande faite a partir de chatgpt qui permet de modifier réactivement le texte
-                text_commande = " \n ".join([f"{value} $" if key == "price" else f"{value}" for key, value in commande.items() if key != "temps"])
-
-            en_cours_frame = Frame(process_frame, bg="#1A355B", width=200, height=200
-                                   , highlightbackground="black", highlightcolor="black", highlightthickness=1)
-            en_cours_frame.pack(pady=10)
-
-            en_cours_label = Label(en_cours_frame, text=text_commande, font=("Helvetica", 10), bg="#1A355B", fg="white"
-                                   , highlightbackground="black", highlightcolor="black", highlightthickness=1)
-            en_cours_label.pack(pady=10, padx=20)
-
-            terminer_button = Button(en_cours_frame, text="Terminer", font=("Helvetica", 10), bg="#1A355B", fg="white"
-                                    , highlightbackground="black", highlightcolor="black", highlightthickness=1
-                                    , command=lambda commande=commande: [accepter_commande(commande), destroy_all_widgets(process_frame)
-                                    ,destroy_all_widgets(commande_frame), add_commandes_en_cours(), add_commandes_terminees()])
-            terminer_button.pack(pady=10)
-    
-    def add_commandes_terminees ():
-        global global_list_commande_terminees
-        for commande in global_list_commande_terminees :
-            if "plat" not in commande : 
-                text_commande = f"{commande['name']} - {commande['price']} €"
-            else :
-                # commande faite a partir de chatgpt qui permet de modifier réactivement le texte
-                text_commande = " \n ".join([f"{value} $" if key == "price" else f"{value}" for key, value in commande.items() if key != "temps"])
-
-            terminees_frame = Frame(commande_frame, bg="#1A355B", width=200, height=200
-                                    , highlightbackground="black", highlightcolor="black", highlightthickness=1)
-            terminees_frame.pack(pady=10)
-
-            terminees_label = Label(terminees_frame, text=text_commande, font=("Helvetica", 10), bg="#1A355B", fg="white"
-                                    , highlightbackground="black", highlightcolor="black", highlightthickness=1)
-            terminees_label.pack(pady=30, padx=20)
-    
-
-
-
-
+    add_commandes(demande_frame, process_frame)
     operateur_window.mainloop()
-
-    
 
 main_operateur_window()
