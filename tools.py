48cb9b89e5af4ed51107c73bccd38542653795af
clemdeboisfleury <deboisfleuryclement@gmail.com>
2025-03-03 14:33:55 +0100

    refresh_page

--- tools.py
+++ tools.py
@@ -2,4 +2,7 @@ def destroy_all_widgets(window):
     for widget in window.winfo_children():
         widget.destroy()
 
+def refresh_page(page, def_redislaye_page) : 
+    destroy_all_widgets(page)
+    def_redislaye_page()
 
